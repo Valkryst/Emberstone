e265b5e580301bfed56efd54ec9574af3daf0291
Valkryst <valkryst@valkryst.com>
2019-03-17 15:26:29 -0300

    Adds the PNG/JSON files for the game's UI. Adds the game's UI to the LevelView.

--- src/com/valkryst/Emberstone/mvc/view/LevelView.java
+++ src/com/valkryst/Emberstone/mvc/view/LevelView.java
@@ -1,9 +1,27 @@
 package com.valkryst.Emberstone.mvc.view;
 
+import com.valkryst.Emberstone.mvc.component.Image;
+import com.valkryst.V2DSprite.Sprite;
+import com.valkryst.V2DSprite.SpriteAtlas;
+import com.valkryst.V2DSprite.SpriteSheet;
+import org.json.simple.parser.ParseException;
+
 import java.awt.*;
+import java.io.IOException;
 
 public class LevelView extends View {
-    @Override
-    public void draw(final Graphics2D gc) {
+    /** Constructs a new LevelView. */
+    public LevelView() {
+        // Set up the UI.
+        try {
+            final SpriteAtlas atlas = SpriteAtlas.createSpriteAtlas("gui/Game UI.png", "gui/Game UI.json");
+
+            // Add Background
+            SpriteSheet sheet = atlas.getSpriteSheet("Game UI");
+            Sprite sprite = sheet.getSprite("Background");
+            super.addComponent(new Image(new Point(0, 0), sprite));
+        } catch (final ParseException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
